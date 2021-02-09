42ea1aa6d660352df84d658412fabf553818288e
Ian Benlolo <ian.benlolo@mail.mcgill.ca>
2021-02-09 16:50:03 -0500

    Loosened requirements on ase due to conflict with nomad ase version

--- setup.py
+++ setup.py
@@ -2,32 +2,54 @@ import io
 import os
 import re
 
-import hea
+# import hea
 from setuptools import find_packages, setup
 
 
-def read(filename):
-    filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type('')
-    with open(filename, mode='r', encoding='utf-8') as fd:
-        return re.sub(text_type(r':[a-z]+`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+# def read(filename):
+#     filename = os.path.join(os.path.dirname(__file__), filename)
+#     text_type = type('')
+#     with open(filename, mode='r', encoding='utf-8') as fd:
+#         return re.sub(text_type(r':[a-z]+`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+# 
+# 
+# requirements_txt = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+# 
+# with open(requirements_txt, encoding='utf-8') as fin:
+#     requires = [line.strip() for line in fin if line and line.strip() and not line.strip().startswith('#')]
 
-
-requirements_txt = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-
-with open(requirements_txt, encoding='utf-8') as fin:
-    requires = [line.strip() for line in fin if line and line.strip() and not line.strip().startswith('#')]
-
+requirements = [
+        'numpy>=1.19.2',
+        'sqlalchemy>=1.3.20',
+        'pandas>=1.1.3',
+        'ase',
+        'pymatgen>=2020.10.20',
+        'scipy>=1.5.2',
+        'h5py>=2.10.0',
+        'loguru>=0.5.3',
+        'pyyaml>=5.3.1',
+        'matminer>=0.6.4',
+        'uncertainties>=3.1.',
+        'pymysql>=0.10.1',
+        'mysql-connector-python>=8.0.22',
+        'matplotlib>=3.3.2',
+        'seaborn>=0.11.0',
+        'paramiko>=2.7.2',
+        'scp>=0.13.3',
+        'cython>=0.29.21',
+        'tensorflow>=2.2.0',
+#        'tensorflow-gpu>=2.2.0',
+        'pexpect>=4.8.0',
+        'nomad-lab==0.9.10',
+        ]
 setup(
-    name='OpenMAP',
+    name='openmap',
     url='https://github.com/CLEANit/OpenMAP',
-    version=hea.__version__,
-    author=hea.__author__,
-    author_email=hea.__email__,
-    description=hea.__doc__,
-    long_description=read('README.rst'),
+    version='1.0',
+    author=['Conrad Tetsassi', 'Ian Benlolo'],
+    author_email='ian.benlolo@uottawa.ca',
     packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=requires,
+    install_requires=requirements,
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
